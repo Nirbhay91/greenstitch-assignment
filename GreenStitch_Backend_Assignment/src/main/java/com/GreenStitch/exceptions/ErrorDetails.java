d429d98d563bd9f40db6b2e8424a29be70d30a53
nirbhaykaushik44@gmail.com <Nirbhay91>
2023-09-08 02:27:29 +0530

    git commit

---
+++ GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/exceptions/ErrorDetails.java
@@ -0,0 +1,38 @@
+package com.GreenStitch.exceptions;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+
+	private LocalDateTime timestamp;
+	private String message;
+	private String description;
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
---
+++ GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/jwt/SecurityConstants.java
@@ -0,0 +1,7 @@
+package com.GreenStitch.jwt;
+
+public interface SecurityConstants {
+
+	public static final String JWT_KEY ="OdOUsLMifpW79SSFo1hGlY-ffidSW88GT5BKmjAIGfM";
+	public static final String JWT_HEADER ="Authorization";
+}
